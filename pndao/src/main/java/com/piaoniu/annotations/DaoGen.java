788e4d484255597021495ac51635d8e4e30fd75b
yihua.huang <code4crafter@gmail.com>
2016-10-30 16:45:53 +0800

    init

---
+++ pndao/src/main/java/com/piaoniu/annotations/DaoGen.java
@@ -0,0 +1,44 @@
+package com.piaoniu.annotations;
+
+public @interface DaoGen {
+    String[] insertPrefix() default {"insert"};
+
+    String[] batchInsertPrefix() default {"batchInsert"};
+
+    String[] findPrefix() default {"findBy"};
+
+    String[] removePrefix() default {"removeBy"};
+
+    String[] queryPrefix() default {"queryBy"};
+
+    String[] queryInPrefix() default {"queryIn"};
+
+    String[] updatePrefix() default {"update"};
+
+    String[] updateForPrefix() default {"updateFor"};
+
+    String[] countPrefix() default {"countBy"};
+
+    String[] countInPrefix() default {"countIn"};
+
+    String[] countAllPrefix() default {"countAll"};
+
+    String[] queryAllPrefix() default {"queryAll"};
+
+    //String[] count() default {"countBy"};
+    String separator() default "And";
+
+    String tablePrefix() default "PN_";
+
+    String primaryKey() default "id";
+
+    String createTime() default "addTime";
+
+    String updateTime() default "updateTime";
+
+    String tableName() default "";
+
+    String orderBy() default "OrderBy";
+
+    String orderByWith() default "With";
+}
---
+++ pndao/src/main/java/com/piaoniu/generator/dao/DaoEnv.java
@@ -0,0 +1,50 @@
+package com.piaoniu.generator.dao;
+
+import com.piaoniu.annotations.DaoGen;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Type;
+
+public class DaoEnv {
+    private DaoGen daoGen;
+    private String daoClassName;
+    private String tableName;
+    private String createTime;
+    private String updateTime;
+    Type typeParameter;
+
+    public DaoEnv(DaoGen daoGen,Symbol.ClassSymbol classSymbol) {
+        this.daoGen = daoGen;
+        createTime = daoGen.createTime();
+        updateTime = daoGen.updateTime();
+        daoClassName = classSymbol.getSimpleName().toString();
+        if (!daoGen.tableName().isEmpty())
+            tableName = daoGen.tablePrefix()+daoGen.tableName();
+            //"Activity{Dao}"
+        else tableName = daoGen.tablePrefix()+daoClassName.subSequence(0,daoClassName.length()-3);
+        if (classSymbol.getInterfaces() != null){
+            classSymbol.getInterfaces().forEach(i->{
+                if (i.getTypeArguments()!=null && !i.getTypeArguments().isEmpty()){
+                    if (i.getTypeArguments().size()> 1 || typeParameter != null) throw new RuntimeException("不支持多个类型参数");
+                    typeParameter = i.getTypeArguments().get(0);
+                }
+            });
+        }
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public Symbol.TypeSymbol getRealTypeByTypeParameter(Type type) {
+        if (typeParameter == null) throw new RuntimeException("未从类定义中获取类型变量实例,请联系技术支持");
+        return typeParameter.tsym;
+    }
+}
