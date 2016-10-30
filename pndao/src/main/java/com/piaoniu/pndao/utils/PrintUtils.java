8612aebc68c8668cfd352552c5743d69515a170d
yihua.huang <code4crafter@gmail.com>
2016-10-30 20:33:02 +0800

    print

--- pndao/src/main/java/com/piaoniu/pndao/utils/PrintUtils.java
+++ pndao/src/main/java/com/piaoniu/pndao/utils/PrintUtils.java
@@ -2,6 +2,6 @@ package com.piaoniu.pndao.utils;
 
 public class PrintUtils {
     public static void note(String data){
-        System.out.println("[note] PN_Plugin: "+data);
+        System.out.println("[note] pndao: "+data);
     }
 }
